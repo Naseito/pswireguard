e454dd7f70988fe2ef79d558897658016cee66ea
Naseito <nasprivnet@duck.com>
2023-03-28 15:49:53 +0200

    First release

---
+++ New-WireguardServer.ps1
@@ -0,0 +1,171 @@
+param ([parameter(Mandatory = $true, HelpMessage = "IPv4 Address of Wireguard server without netmask.")][string] $IPv4ServerAddress,
+    [parameter(Mandatory = $true, HelpMessage = "Netmask of the wireguard server.Accept values from 0 to 32")][Int32] $IPv4NetMask,
+    [parameter(Mandatory = $true, HelpMessage = "IPv4 Address of the DNS server that will be forwarded to the VPN client")][string] $IPv4DNS,
+    [parameter(Mandatory = $true, HelpMessage = "Port where Wireguard will listen. Don´t forget to open the port in your router!")][string] $IPv4UDPPort,
+    [parameter(Mandatory = $true, HelpMessage = "Name of the interface of the machine where wireguard will route to. Eg: eth0")][string] $InterfaceName,
+    [parameter(Mandatory = $true, HelpMessage = "Extermal IPv4/FQDN for the VPN Server. Eg: vpn.somedomain.com")][string] $ServerEndpointName,
+    [parameter(Mandatory = $true, HelpMessage = "Enter the number of config files for clients that you would like to generate")][Int32] $LicensesToGenerate,
+    [parameter(ParameterSetName = 'Routing', HelpMessage = "If set, you will be allowed to enter the subnet desired for VPN routing using parameter -LANRoutingSubnet with CIDR format")][switch] $OnlyLANRouting,
+    [parameter(ParameterSetName = 'Routing', HelpMessage = "Enter the subnet desired for VPN routing with CIDR format.Eg: 10.33.0.0/16")][string] $LANRoutingSubnet
+)
+
+# Dependencies
+
+if (Get-Module -ListAvailable -Name Subnet) {
+    Write-Host "Subnet Module exists on the system" -ForegroundColor Green
+    Import-Module Subnet 
+    
+} 
+else {
+    Write-Host "Subnet module not present. Installing..." -ForegroundColor Yellow
+    Install-Module Subnet -AllowClobber -Confirm:$False -Force
+    Import-Module Subnet
+    Exit
+}
+if (Get-Module -ListAvailable -Name QRCodeSt) {
+    Write-Host "Subnet Module exists on the system" -ForegroundColor Green
+    Import-Module QRCodeSt
+        
+} 
+else {
+    Write-Host "QRCodeSt module not present. Installing..." -ForegroundColor Yellow
+    Install-Module QRCodeSt -AllowClobber -Confirm:$False -Force
+    Import-Module QRCodeSt
+    Exit
+}
+
+
+
+#Calculate subnet
+$networkInfo = Get-Subnet -IP $IPv4ServerAddress -MaskBits $IPv4NetMask
+
+# Checks if parameters received are correct
+
+## Checks that $IPv4ServerAddress, $IPv4NetMask and $IPv4DNS are actual IPv4 addresses 
+$IPpattern = "^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$"
+if ($IPv4ServerAddress -notmatch $IPpattern -or $IPv4DNS -notmatch $IPpattern) {
+    Write-Error "Either $IPv4ServerAddress or $IPv4DNS dont comply with the IPv4 format. Exiting script..."
+    Exit 
+}
+
+if ($IPv4NetMask -notin 0..32) {
+    Write-Error "Incorrect Network Mask. Exiting script..." 
+    Exit 
+}
+
+## Checks that there are enough IPs on the subnet for the number of licenses desired.
+
+if ($LicensesToGenerate -gt $networkInfo.HostAddressCount) {
+    Write-Error "The number of licenses to generate ($LicensesToGenerate) is higher than the available host address for the subnet ($($networkInfo.HostAddressCount)). Exiting script..."
+    Exit
+}
+
+
+
+Write-Verbose "Generating qrcode for $($configFile.Name)"
+if ($IsLinux) {
+    # Public, Private and Preshare Key Generation - Server Side
+    bash -c  "wg genkey > Sprivatekey.key"
+    bash -c  "wg pubkey < Sprivatekey.key > Spublickey.pub"
+    bash -c  "wg genpsk > sharedkey.key"
+    # Creating Interface - Server Side
+    bash -c  "wg genkey | tee wg-private.key | wg pubkey > wg-public.key"
+}
+elseif ($IsWindows) {
+    # Public, Private and Preshare Key Generation - Server Side
+    cmd /c  "wg genkey > Sprivatekey.key"
+    cmd /c  "wg pubkey < Sprivatekey.key > Spublickey.pub"
+    cmd /c  "wg genpsk > sharedkey.key"
+    # Creating Interface - Server Side
+    wg genkey | Tee-Object "wg-private.key" | wg pubkey | Out-File "wg-public.key"
+}
+
+# Create config file (if needed)
+if (-Not (Test-Path "wg0.conf")) {
+    New-Item "wg0.conf" -Force
+}
+
+Add-Content -Path "wg0.conf" -Value "[Interface]"
+Add-Content -Path "wg0.conf" -Value "Address = $IPv4ServerAddress/$IPv4NetMask"
+Add-Content -Path "wg0.conf" -Value "SaveConfig = true"
+Add-Content -Path "wg0.conf" -Value 'PostUp = /etc/wireguard/up.sh'
+Add-Content -Path "wg0.conf" -Value 'PostDown = /etc/wireguard/down.sh'
+Add-Content -Path "wg0.conf" -Value "ListenPort = $IPv4UDPPort"
+Add-Content -Path "wg0.conf" -Value $("PrivateKey = " + $(Get-Content  -Path "Sprivatekey.key"))
+Add-Content -Path "wg0.conf" -Value "#"
+
+For ($i = 1; $i -le $LicensesToGenerate + 1; $i++) {
+    Write-Verbose "Generating license number $($i+1)"
+    
+    #Generate keys for client interface
+    Write-Verbose "Generating qrcode for $($configFile.Name)"
+    if ($IsLinux -eq $true) {
+        bash -c "wg genkey > Cprivatekey.key"
+        bash -c "wg pubkey < Cprivatekey.key > Cpublickey.pub"
+    }
+    elseif ($IsWindows -eq $true) {
+        cmd /c "wg genkey > Cprivatekey.key"
+        cmd /c "wg pubkey < Cprivatekey.key > Cpublickey.pub"
+    }
+
+
+    #Adding peer to server config file
+    Add-Content -Path "wg0.conf" -Value "[Peer]"
+    Add-Content -Path "wg0.conf" -Value "# client $i"
+    Add-Content -Path "wg0.conf" -Value $("PublicKey = " + $(Get-Content Cpublickey.pub))
+    Add-Content -Path "wg0.conf" -Value $("PresharedKey = " + $(Get-Content sharedkey.key))
+    Add-Content -Path "wg0.conf" -Value $("AllowedIPs = " + "$($networkInfo.HostAddresses[$i])/32")
+    Add-Content -Path "wg0.conf" -Value "#"       
+    Add-Content -Path "wg0.conf" -Value "#"
+
+    #Client config generation
+    New-Item -Path "client$($i+1).conf" -Force
+    Add-Content -Path "client$($i+1).conf" -Value "[Interface]"
+    Add-Content -Path "client$($i+1).conf" -Value $("Address =" + "$($networkInfo.HostAddresses[$i])/$IPv4NetMask")
+    Add-Content -Path "client$($i+1).conf" -Value $("PrivateKey = " + $(Get-Content Cprivatekey.key))
+    Add-Content -Path "client$($i+1).conf" -Value $("DNS = " + $IPv4DNS)
+    Add-Content -Path "client$($i+1).conf" -Value "#"
+    Add-Content -Path "client$($i+1).conf" -Value "[Peer]"
+    Add-Content -Path "client$($i+1).conf" -Value $("PublicKey = " + $(Get-Content Spublickey.pub))
+    Add-Content -Path "client$($i+1).conf" -Value $("PresharedKey = " + $(Get-Content sharedkey.key))
+    if ($OnlyLANRouting) {
+        Add-Content -Path "client$($i+1).conf" -Value "AllowedIPs = $LANRoutingSubnet"
+    }
+    else {
+        Add-Content -Path "client$($i+1).conf" -Value "AllowedIPs = 0.0.0.0/0"
+    }
+    Add-Content -Path "client$($i+1).conf" -Value "Endpoint = $($ServerEndpointName):$($IPv4UDPPort)"
+    Add-Content -Path "client$($i+1).conf" -Value "#"
+}
+
+# Create QR codes for each client config file
+$configFiles = Get-ChildItem | Where-Object { $_.Name -like "client*" }
+foreach ($configFile in $configFiles) {
+    Write-Verbose "Generating qrcode for $($configFile.Name)"
+    New-QRCodeSt -Info $($(Get-Content $configFile.FullName | Out-String)) -OutPath "$(Get-Location)\$($configFile.Name).png"  
+}
+
+# Create up and down files (if needed)
+if (-Not (Test-Path "up.sh")) {
+    New-Item "up.sh" -Force
+}
+if (-Not (Test-Path "down.sh")) {
+    New-Item "down.sh" -Force
+}
+Add-Content -Path "up.sh" -Value "ufw route insert 1 allow in on wg0 out on $InterfaceName"
+Add-Content -Path "up.sh" -Value "ufw route insert 1 allow in on $InterfaceName out on wg0"
+Add-Content -Path "down.sh" -Value "ufw route delete allow in on wg0 out on $InterfaceName"
+Add-Content -Path "down.sh" -Value "ufw route delete allow in on $InterfaceName out on wg0"
+
+# If you are on a Ubuntu machine/vm, copy the files and enable the service. Needs root user.
+if ($IsLinux -and $PSVersionTable.OS -like "*Ubuntu*" -and $(Get-ChildItem Env:\USER).Value -eq "root") {
+    apt install wireguard -y
+    Copy-Item -Path "up.sh" -Destination "/etc/wireguard/" -Force
+    Copy-Item -Path "down.sh" -Destination "/etc/wireguard/" -Force
+    Copy-Item -Path "wg0.conf" -Destination "/etc/wireguard/" -Force
+    chmod +x /etc/wireguard/up.sh
+    chmod +x /etc/wireguard/down.sh
+    (Get-Content -Path /etc/sysctl.conf) | ForEach-Object { $_ -replace '^#\s*net.ipv4.ip_forward=1', 'net.ipv4.ip_forward=1' } | Set-Content -Path /etc/sysctl.conf
+    systemctl enable wg-quick@wg0.service
+    systemctl start wg-quick@wg0.service
+}
--- README.md
+++ README.md
@@ -1,2 +1,101 @@
 # pswireguard
-PowerShell helper script for creating wireguard VPN server and client configuration.
+
+PowerShell helper script for creating wireguard VPN server and client configuration. Only for IPv4. Feel free to fork and adapt to your needs. As I wanted to be as simple as possible, the basic up and down rules are created with ufw.
+
+## Basics
+
+The script behave slightly different depending on the OS.
+
+- **Windows:** Creates config files for server, clients and qr codes for the clients
+- **Any Linux or Ubuntu with no root user:** Creates config files for server, clients and qr codes for the clients
+- **Ubuntu with root user:** Creates config files for server, clients and qr codes for the clients. Installs wireguard, copy the config files to /etc/wireguard and enables/starts the service.
+
+## Parameters
+
+### Mandatory
+
+- **IPv4ServerAddress:** [String] The address space (without netmask) for the wireguard virtual network. Eg: "10.44.0.0"
+- **IPv4NetMask:** [Int] The netmask for the wireguard virtual network. Eg: 24
+- **IPv4DNS:** [String] The IP address of your internal DNS server. Eg: "10.40.0.250"
+- **IPv4UDPPort:** [String] The port where the wireguard server will listen to Eg: "51820"  
+- **InterfaceName:** [String] Name of the interface of the machine where wireguard will route to. Eg: "eth0"
+- **ServerEndpointName:** [String] Extermal IPv4/FQDN for the VPN Server. Eg: "vpn.somedomain.com"
+- **LicensesToGenerate:** [Int] Enter the number of config files for clients that you would like to generate. Limited by the number of possible hosts with the IPv4ServerAddress/IPv4NetMask combination Eg: 10
+
+### Optional
+
+- **OnlyLANRouting:** [Switch] If this parameter is passed, LANRoutingSubnet will be used in the client config files to limit the cliens "Allowed Ips". If this is not passed all clients will be created with "Allowed Ips" set to 0.0.0.0/0.
+- **LANRoutingSubnet:** [String] Enter the subnet desired for VPN routing with CIDR format. Requires OnlyLANRouting to be effective. Eg: "10.40.0.0/24"
+- **Verbose:** For a better output of the script.
+
+### Example
+
+From a PowerShell terminal:
+
+``` PowerShell
+./Get-WireguardLicenses.ps1 -IPv4ServerAddress "10.44.0.0" -IPv4NetMask 24 -IPv4DNS "10.40.0.250" -IPv4UDPPort "51820" -ServerEndpointName "vpn.somedomain.com" -LicensesToGenerate 10 -OnlyLANRouting -LANRoutingSubnet "10.40.0.0/24" -Verbose
+```
+
+### Resulting files
+
+| File | Description |
+| ----------- | ----------- |
+| client*.conf | These are the files that you will later need to import to your clients. |
+| client*.png | QR codes with client configuration in png format for easy sharing. |
+| wg0.conf | This file has the configuration of the wireguard server. In Ubuntu it should be place in /etc/wireguard/ -- Also the name of the file will be used later for enabling/start/stop/restart the service (Eg: systemctl start wg-quick@wg0.service)|
+| up.sh | The script that will be run when starting the wireguard server. In Ubuntu it should be place in /etc/wireguard/ -- Read the routing section of this readme file for more information. |
+| down.sh | The script that will be run when stopping the wireguard server. In Ubuntu it should be place in /etc/wireguard/ -- Read the routing section of this readme file for more information. |
+| .pub and .key | The public and private certificates used for generating the configuration. Please store them safely. |
+
+## Routing
+
+### Default routing
+
+If you are using ubuntu and running the script as root, everything necessary to do the basic routing will be done automatically. In case you are running it in another Linux distro or another user than root, here is what you need to do:
+
+- Uncomment the line #net.ipv4.ip_forward=1 on /etc/sysctl.conf
+- Copy up.sh and down.sh to a location of your desire. Change in wg0.conf the path for the scripts and don´t forget to give execution permissions to the scripts.
+
+>chmod +x /etc/wireguard/up.sh && chmod +x /etc/wireguard/down.sh
+
+- Enable and start the service:
+
+>systemctl enable wg-quick@wg0.service && systemctl start wg-quick@wg0.service
+
+The default routing will allow all traffic between the host interface and the wireguard interface:
+
+`ufw route insert 1 allow in on wg0 out on eth0`
+
+`ufw route insert 1 allow in on eth0 out on wg0`
+
+### Advance routing
+
+With ufw is quite easy to create custom routing rules. You may want some clients to have full access (your LAN + Internet), others to have only restricted access to some of your LAN services, and maybe others with Internet redirection but no access to the LAN. I will provide some examples, but the most important thing is the order of the rules. Rules are executed by order, if the order is not correct the routing may fail (that´s the reason why I use ufw route insert 1, to always put the rule in the first place).
+
+**Put the rules in up.sh in reverse order**  (the first ones in up.sh will be the last to be checked on ufw rule list)
+
+The first two rules should be:
+
+Allow all routing from host interface to wireguard interface:
+
+`ufw route insert 1 allow in on eth0 out on wg0`
+
+Deny all routing from wireguard interface to host interface:
+
+`ufw route insert 1 deny in on wg0 out on eth0`
+
+Allow all wireguard clients to communicate with the DNS server (10.40.0.250):
+
+`ufw route insert 1 allow in on wg0 out on enp3s0 to 10.40.0.250 port 53 from 10.44.0.0/24`
+
+Allow one client (10.44.0.3) to communicate with the LAN and also routing internet:
+
+`ufw route insert 1 allow in on wg0 out on eth0 to any from 10.44.0.3`
+
+Allow a specific client (10.44.0.2) to access a NAS with SMB (10.40.0.4) and then block the rest of the routing to the LAN.
+
+`ufw route insert 1 deny in on wg0 out on eth0 to 10.40.0.0/24`
+
+`ufw route insert 1 allow in on wg0 out on eth0 to 10.40.0.4 port 445 from 10.44.0.2`
+
+Then copy the same rules on down.sh replacing **"insert 1"** by **"delete"**.
